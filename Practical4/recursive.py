4e12e10eea5653f63c8b70de849fff86b61d3694
Coell Earth <3277145769@qq.com>
2024-03-04 23:02:00 +0800

    completed the task in recursive sequence

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,2 +1,11 @@
-
+#the first number is 4
+#do the following command for 4 times to get a 5 number sequence:
+  #multiply the former number by 2 and plus one
+  #output the sequence together with the starting number 4
+print("The recursive sequence is")
+a=4
+print(a)
+for i in range (5):
+    a=a*2+3
+    print(a)
 
