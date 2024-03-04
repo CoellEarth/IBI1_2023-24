12ab5e5723954f216279399a26d911cdbe8f7f6c
Coell Earth <3277145769@qq.com>
2024-03-04 20:30:50 +0800

    completed_tasks

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,1 +1,24 @@
+#Task 1:some simple math
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+#compare d and e
+if d>e:
+    print("running is better")
+else:
+    print("combine running and strength training is better")
+#for that e>d so combining running and strength training is better than running alone
+
+#Task 2:Booleans
+X=True
+Y=False
+W=X ^ Y
+#Truth table of W
+#X      Y      W
+#true  true  false
+#false false false
+#true  false true
+#false true  false
 
