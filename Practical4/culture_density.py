99cf632bfae1f7274189f4cf2978daf17cbe352c
Coell Earth <3277145769@qq.com>
2024-03-04 22:37:22 +0800

    completed the task in culture density

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,1 +1,15 @@
+#initial cell density=5
+#the starting day=0
+#when the density hasn't reached 90, implement the steps below everytime
+#   doubles the density
+#   ad1 to the day number
+#at last output the day
+celldensity=5
+day=0
+while celldensity<=90:
+    celldensity=celldensity*2
+    day=day+1
+print("On day",day,"the cell density goes over 90%, which means you have",day-1,"free days to enjoy your holiday")
+   
+    
 
