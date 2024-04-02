09aac9da3e1f0067898d90457a5578de5c918695
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-04-02 23:17:16 +0800

    Update repetitive_counter.py

--- Practical8/repetitive_counter.py
+++ Practical8/repetitive_counter.py
@@ -1,1 +1,11 @@
-
+seq="ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA"
+GTGTGT_Count=0
+GTCTGT_Count=0
+length_seq=len(seq)
+for start in range(length_seq):
+    if seq[start:start+6]=="GTGTGT":
+        GTGTGT_Count=GTGTGT_Count+1
+    elif seq[start:start+6]=="GTCTGT":
+        GTCTGT_Count=GTCTGT_Count+1
+print("The GTGTGT count is ",GTGTGT_Count)
+print("The GTCTGT count is ",GTCTGT_Count)
