fa526228562f6b45ad3e35fd7ca74984f947a29d
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-04-08 23:52:27 +0800

    Update Favorite_JamesBond.py

--- Practical9/Favorite_JamesBond.py
+++ Practical9/Favorite_JamesBond.py
@@ -1,1 +1,15 @@
-
+str_born=input("You were born in year:")
+born=int(str_born)
+def your_fav_JamesBond(born):
+    if born+18<=1986 and born+18>=1973:
+        return "Roger Moore"
+    elif born+18<=1994 and born+18>=1987:
+        return "Timothy Dalton"
+    elif born+18<=2005 and born+18>=1995:
+        return "Pierce Brosnan"
+    elif born+18<=2021 and born+18>=2006:
+        return "Daniel Craig"
+    else:
+        return"no one, because James Bond does not exist in your era"
+    
+print("Your fav James Bond might be",your_fav_JamesBond(born))
