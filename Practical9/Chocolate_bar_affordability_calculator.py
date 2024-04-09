c139fe29ab1191bb9af7d2571cfc6e31173ed6af
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-04-09 10:24:45 +0800

    add results to Chocolate_bar_affordability_calculator.py

--- Practical9/Chocolate_bar_affordability_calculator.py
+++ Practical9/Chocolate_bar_affordability_calculator.py
@@ -7,3 +7,4 @@ def affordability(total_money, price_per_piece):
 total_money = 100
 price_per_piece = 7
 print(f"Based on the information that you have {total_money} yuan in total and each piece of chocolate bar costs {price_per_piece} yuan, so {affordability(total_money, price_per_piece)}")
+#the output is "Based on the information that you have 100 yuan in total and each piece of chocolate bar costs 7 yuan, so you can afford 14 piece(s) of chocolate bar. And you still have 2 yuan left."
