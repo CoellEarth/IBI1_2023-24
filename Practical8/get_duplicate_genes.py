89025329c4a1568002cce2c629e4b6ea51d93160
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-04-10 20:50:55 +0800

    Update get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,1 +1,26 @@
+import re
+
+input_file = r"C:\Users\32771\Desktop\IBI\practical\materials\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+output_file = "second_file.fasta"
+
+with open(input_file, 'r') as f:
+    content = f.read()
+
+
+with open(output_file, 'w') as t:
+    t.write(content)
+
+with open(output_file, "+r") as g:
+    modified_content = g.read()
+    third_modified_content_part = modified_content.split(">")
+    duplicated_genes = []
+    for part in third_modified_content_part:
+        if "duplication" in part:
+            start_index = part.find("]")
+            gene_names = ">"+part.split(" ")[0]
+            gene_sequence = part[start_index + 1:]
+            duplicated_genes.append(gene_names + "\n" + gene_sequence)
+
+for gene_info in duplicated_genes:
+    print(gene_info)
 
