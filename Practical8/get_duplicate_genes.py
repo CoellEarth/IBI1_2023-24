cd80fee285275c837a1ed8cebf35476fd5e4bc2c
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-05-15 23:30:13 +0800

    Update get_duplicate_genes.py

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,30 +1,25 @@
-import re
+#imput the word to be checked
+word ='duplication'
+#open the file
+with open(r"E:\IBI1_2023-24\Practical8\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", "r") as input_file, open(r"E:\IBI1_2023-24\Practical8\duplicate genes.fa", "w") as output_file:
+    check_duplication = False
+    gene_description =''
+    gene_sequence = ''
 
-input_file = r"C:\Users\32771\Desktop\IBI\practical\materials\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
-output_file = "duplicated_genes.fa"
-
-with open(input_file, 'r') as f:
-    content = f.read()
-
-
-with open(output_file, 'w') as t:
-    t.write(content)
-
-with open(output_file, "a") as g:
-    third_modified_content_part = content.split(">")
-    duplicated_genes = []
-    for part in third_modified_content_part:
-        if "duplication" in part:
-            start_index = part.find("]")
-            gene_names = ">"+part.split(" ")[0]
-            gene_sequence = part[start_index + 1:]
-            g.write(gene_names + gene_sequence+"\n")
-
-
-
-
-
-
-
-
-
+    for line in input_file:
+        if line.startswith('>'):
+            #checked if the gene sequence is duplicated, if yes, write its information into the output file
+            if word in line:
+                check_duplication = True
+                gene_description = line.strip()
+                gene_name = gene_description.split()[0][1:]
+                output_file.write(f">{gene_name}\n")
+            #if not duplicated, reset the gene description and sequence
+            else:
+                check_duplication= False
+                gene_description =''
+                gene_sequence = ''
+        #if it is the sequence line, and has duplication, write into the output file
+        elif check_duplication:
+            gene_sequence += line.strip()
+            output_file.write(line)
