4bf5215e802bf34ee76178598f47126a3c5158fd
Coell Earth <161328923+CoellEarth@users.noreply.github.com>
2024-05-18 22:07:11 +0800

    refine Alignment.py

--- Practical13/Alignment.py
+++ Practical13/Alignment.py
@@ -1,47 +1,29 @@
-###this file only includes the code, the answers to the quetion are in another file in this dir.
-
+#import the modules to be used, and use blosum to read the BLOSUM62 matrix
+import re
 import blosum as bl
 matrix = bl.BLOSUM(62)
 
+#input the sequences to be checked
 human = 'METTPLNSQKQLSACEDGEDCQENGVLQKVVPTPGDKVESGQISNGYSAVPSPGAGDDTRHSIPATTTTLVAELHQGERETWGKKVDFLLSVIGYAVDLGNVWRFPYICYQNGGGAFLLPYTIMAIFGGIPLFYMELALGQYHRNGCISIWRKICPIFKGIGYAICIIAFYIASYYNTIMAWALYYLISSFTDQLPWTSCKNSWNTGNCTNYFSEDNITWTLHSTSPAEEFYTRHVLQIHRSKGLQDLGGISWQLALCIMLIFTVIYFSIWKGVKTSGKVVWVTATFPYIILSVLLVRGATLPGAWRGVLFYLKPNWQKLLETGVWIDAAAQIFFSLGPGFGVLLAFASYNKFNNNCYQDALVTSVVNCMTSFVSGFVIFTVLGYMAEMRNEDVSEVAKDAGPSLLFITYAEAIANMPASTFFAIIFFLMLITLGLDSTFAGLEGVITAVLDEFPHVWAKRRERFVLAVVITCFFGSLVTLTFGGAYVVKLLEEYATGPAVLTVALIEAVAVSWFYGITQFCRDVKEMLGFSPGWFWRICWVAISPLFLLFIICSFLMSPPQLRLFQYNYPYWSIILGYCIGTSSFICIPTYIAYRLIITPGTFKERIIKSITPETPTEIPCGDIRLNAV'
 mouse = 'METTPLNSQKVLSECKDKEDCQENGVLQKGVPTPADKAGPGQISNGYSAVPSTSAGDEAPHSTPAATTTLVAEIHQGERETWGKKMDFLLSVIGYAVDLGNIWRFPYICYQNGGGAFLLPYTIMAIFGGIPLFYMELALGQYHRNGCISIWKKICPIFKGIGYAICIIAFYIASYYNTIIAWALYYLISSFTDQLPWTSCKNSWNTGNCTNYFAQDNITWTLHSTSPAEEFYLRHVLQIHQSKGLQDLGTISWQLALCIMLIFTIIYFSIWKGVKTSGKVVWVTATFPYIVLSVLLVRGATLPGAWRGVVFYLKPNWQKLLETGVWVDAAAQIFFSLGPGFGVLLAFASYNKFNNNCYQDALVTSVVNCMTSFVSGFVIFTVLGYMAEMRNEDVSEVAKDAGPSLLFITYAEAIANMPASTFFAIIFFLMLITLGLDSTFAGLEGVITAVLDEFPHIWAKRREWFVLIVVITCILGSLLTLTSGGAYVVTLLEEYATGPAVLTVALIEAVVVSWFYGITQFCSDVKEMLGFSPGWFWRICWVAISPLFLLFIICSFLMSPPQLRLFQYNYPHWSIILGYCIGTSSVICIPIYIIYRLISTPGTLKERIIKSITPETPTEIPCGDIRMNAV'
 rat = 'METTPLNSQKVLSECKDREDCQENGVLQKGVPTTADRAEPSQISNGYSAVPSTSAGDEASHSIPAATTTLVAEIRQGERETWGKKMDFLLSVIGYAVDLGNIWRFPYICYQNGGGAFLLPYTIMAIFGGIPLFYMELALGQYHRNGCISIWRKICPIFKGIGYAICIIAFYIASYYNTIIAWALYYLISSLTDRLPWTSCTNSWNTGNCTNYFAQDNITWTLHSTSPAEEFYLRHVLQIHQSKGLQDLGTISWQLTLCIVLIFTVIYFSIWKGVKTSGKVVWVTATFPYIVLSVLLVRGATLPGAWRGVVFYLKPNWQKLLETGVWVDAAAQIFFSLGPGFGVLLAFASYNKFNNNCYQDALVTSVVNCMTSFVSGFVIFTVLGYMAEMRNEDVSEVAKDAGPSLLFITYAEAIANMPASTFFAIIFFLMLITLGLDSTFAGLEGVITAVLDEFPHIWAKRREWFVLIVVITCVLGSLLTLTSGGAYVVTLLEEYATGPAVLTVALIEAVAVSWFYGITQFCSDVKEMLGFSPGWFWRICWVAISPLFLLFIICSFLMSPPQLRLFQYNYPHWSIVLGYCIGMSSVICIPTYIIYRLISTPGTLKERIIKSITPETPTEIPCGDIRMNAV'
 
-alignment_score = 0
-identical_count = 0
+#create a functioon for alignment_score and identical_count for the two sequences
+def Similarity_assessment(seq1_name,seq1,seq2_name,seq2):
+    #initialize the values
+    alignment_score = 0
+    identical_count = 0
+    #create loops for each amino acid in the seq1
+    for i in range(len(seq1)):
+        #if the amino acid is the same, +1 to identical_count
+        if seq1[i] == seq2[i]:
+            identical_count += 1
+        #for each pair of amino acid, add the corresponding alignment_score
+        alignment_score += matrix[seq1[i]][seq2[i]]
+    percentage_identity = (identical_count / len(human)) * 100
+    #return the result of the test
+    return f' the identity percentage is {percentage_identity}% between {seq1_name} and {seq2_name} and the alignment_score is {alignment_score} between {seq1_name} and {seq2_name}'
 
-for i in range(len(human)):
-    if human[i] == mouse[i]:
-        identical_count += 1
-    alignment_score += matrix[human[i]][mouse[i]]
-percentage_identity = (identical_count / len(human)) * 100
-print("split line 1")
-print("There are",identical_count,"same aa in human and mouse")
-print('the percentage identity is',percentage_identity/100,'between human and mouse ')
-print('the alignment_score is',alignment_score,'between human and mouse')
-alignment_score = 0
-identical_count = 0
-
-for i in range(len(human)):
-    if human[i] == rat[i]:
-        identical_count += 1
-    alignment_score += matrix[human[i]][rat[i]]
-percentage_identity = (identical_count / len(human)) * 100
-print("split line 2")
-print("There are",identical_count,"same aa in human and rat")
-print('the percentage identity is',percentage_identity/100,'between human and rat ')
-print('the alignment_score is',alignment_score,'between human and rat')
-alignment_score = 0
-identical_count = 0
-
-for i in range(len(mouse)):
-    if mouse[i] == rat[i]:
-        identical_count += 1
-    alignment_score += matrix[mouse[i]][rat[i]]
-percentage_identity = (identical_count / len(mouse)) * 100
-print("split line 3")
-print("There are",identical_count,"same aa in mouse and rat")
-print('the percentage identity is',percentage_identity/100,'between mouse and rat ')
-print('the alignment_score is',alignment_score,'between mouse and rat')
-alignment_score = 0
-identical_count = 0
+print(Similarity_assessment('Human',human,'Mouse',mouse))
+print(Similarity_assessment('Human',human,'Rat',rat))
+print(Similarity_assessment('Rat',rat,'Mouse',mouse))
